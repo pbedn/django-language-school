ff3b0332941c724ca02ad811664a8418ea99662c
Paweł Bednarz <pbedn@users.noreply.github.com>
2018-12-14 18:45:54 +0100

    Update project according to two scoops best practice

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# django-language-schoo
+# django-language-school
 
 Application for language school management
 
 ## Setup
 
 ```
-pip install -r doc/requirements/[prod,local].txt
-cp school/settings/local.py school/settings/local_settings.py
-export DJANGO_SETTINGS_MODULE=school.settings.local_settings
+pip install -r requirements/local.txt
+export DJANGO_SETTINGS_MODULE=school.settings.local
 ```
 
 ## Run application
--- school/settings/base.py
+++ school/settings/base.py
@@ -11,9 +11,21 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    """Get the environment variable or return exception."""
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = 'Set the {} environment variable'.format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False
 
@@ -46,7 +58,7 @@ ROOT_URLCONF = 'school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +80,7 @@ WSGI_APPLICATION = 'school.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -110,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static_root'
+STATICFILES_DIRS = [BASE_DIR / 'static']
