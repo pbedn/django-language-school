9d4d5c3e78923b9759d876726605d04cb79045c3
Paweł Bednarz <pbedn@users.noreply.github.com>
2018-12-18 02:11:54 +0100

    Create custom user

--- school/settings/base.py
+++ school/settings/base.py
@@ -44,6 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+PROJECT_APPS = [
+    'school.accounts'
+]
+
+INSTALLED_APPS += PROJECT_APPS
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,6 +61,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'school.urls'
+AUTH_USER_MODEL = 'accounts.User'
 
 TEMPLATES = [
     {
