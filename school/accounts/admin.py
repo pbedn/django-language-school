19af0aaeabc771eb95a1d983c2b0959fc4867fdd
Paweł Bednarz <pbedn@users.noreply.github.com>
2018-12-19 20:29:03 +0100

    Add login through email instead username

--- school/accounts/admin.py
+++ school/accounts/admin.py
@@ -5,7 +5,24 @@ from. models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
