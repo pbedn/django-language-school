c703bfbff7b985e284519032f817bf16a4b9418b
Paweł Bednarz <pbedn@users.noreply.github.com>
2019-01-24 21:08:43 +0100

    Reformat tests with black

--- school/accounts/tests/test_forms.py
+++ school/accounts/tests/test_forms.py
@@ -4,21 +4,24 @@ from django.contrib.auth import get_user_model
 from ..forms import CustomUserCreationForm
 
 
-class CustomUserCreationFormTestCase(TestCase):
+class CustomUserCreationFormTest(TestCase):
     """
     Tests for CustomUser form
     """
+
     def test_valid_data(self):
-        form = CustomUserCreationForm({
-            'email': "test@example.com",
-            'password1': "SuperStrong123###",
-            'password2': "SuperStrong123###",
-        })
+        form = CustomUserCreationForm(
+            {
+                "email": "test@example.com",
+                "password1": "SuperStrong123###",
+                "password2": "SuperStrong123###",
+            }
+        )
         self.assertTrue(form.is_valid())
         comment = form.save()
         self.assertEqual(comment.email, "test@example.com")
         self.user = get_user_model().objects.get(email="test@example.com")
-        self.assertEqual(self.user.check_password('SuperStrong123###'), True)
+        self.assertEqual(self.user.check_password("SuperStrong123###"), True)
 
     def test_blank_data(self):
         CustomUserCreationForm({})
--- school/accounts/tests/test_models.py
+++ school/accounts/tests/test_models.py
@@ -2,14 +2,24 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class CustomUserTestCase(TestCase):
+class CustomUserModelTest(TestCase):
     """
     Tests for the CustomUser model
     """
+
     def setUp(self):
-        self.user = get_user_model().objects.create_user('test@example.com')
+        self.user = get_user_model().objects.create_user("test@example.com")
 
     def test_user_is_created(self):
         user = get_user_model().objects.all()[0]
-        self.assertEqual(user.email, 'test@example.com')
-        self.assertEqual(user.password, getattr(self.user, 'password'))
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.password, getattr(self.user, "password"))
+
+
+class StudentModelTest(TestCase):
+    """
+    Tests for the Student model
+    """
+
+    def setUp(self):
+        pass
