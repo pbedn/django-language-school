8cbe76204c2f7f3f60daeb72716b56250ec3cfff
Paweł Bednarz <pbedn@users.noreply.github.com>
2019-01-27 14:54:52 +0100

    Corrected deployment command
    * add bash script
    * use DEPLOY_COMMAND in CircleCI env variables

--- .circleci/info.md
+++ .circleci/info.md
@@ -1,15 +1,19 @@
 ## CircleCI 2.0 configuration
 
 #### *BUILD SETTINGS / Environment Variables*:
-Set variables
 
-    PROJECT_DIR /home/user/path-to-cloned-repo
+Set variables:
+
     SSH_HOST ip-address-of-server
-    SSH_USER user
+    SSH_USER username
+    SERVER_REPO_PATH <path-to-repo>
+    SERVER_VENV_ACTIVATE_PATH <path-to-venv-activate>
+    SSH_FINGERPRINT <rsa-fingerprint>
 
 #### *PERMISSIONS / checkout SSH keys*
+
 Authorize github deploy
 
 #### *PERMISSIONS / SSH Permissions*
+
 Copy private ssh key from server
-    
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ssh -o StrictHostKeyChecking=no -tv $SSH_USER@$SSH_HOST << EOF
+
+echo "0. Setting virtual env path and repo directory"
+cd $SERVER_REPO_PATH
+source $SERVER_VENV_ACTIVATE_PATH
+echo "1. Fetching all remotes"
+git fetch --all
+echo "2. Checking out using force.."
+git checkout --force origin/master
+echo "3. Updating requirements"
+poetry install
+echo "4. Migrating database"
+./manage.py migrate
+echo "5. Restarting nginx and gunicorn servers"
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
+echo "Done!"
+
+EOF
