aa78cbfa3bcc633c113a1ad82fb5d19d3a68561e
Paweł Bednarz <pbedn@users.noreply.github.com>
2019-01-27 14:09:22 +0100

    Add sentry.io integration

--- school/settings/production.py
+++ school/settings/production.py
@@ -1,5 +1,8 @@
 from .base import *
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
 DATABASES = {
@@ -16,3 +19,8 @@ DATABASES = {
 ALLOWED_HOSTS = [get_env_variable('SERVER_DOMAIN'),
                  get_env_variable('SERVER_IP'),
                  'localhost']
+
+sentry_sdk.init(
+    dsn=get_env_variable('SENTRY_LINK'),
+    integrations=[DjangoIntegration()]
+)
