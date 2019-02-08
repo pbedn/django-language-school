cc72206ce269e6a8a40d7f5758bd98c3b09c7972
Paweł Bednarz <pbedn@users.noreply.github.com>
2019-02-08 12:08:39 +0100

    Harden security on production site

--- school/settings/production.py
+++ school/settings/production.py
@@ -5,6 +5,18 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 SECRET_KEY = get_env_variable("DJANGO_SECRET_KEY")
 
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 3600  # FIXME: increase when site is ready
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
