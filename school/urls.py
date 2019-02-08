bcb28d9354c4a37c802e06c60f9bcc99c69e5d9f
Paweł Bednarz <pbedn@users.noreply.github.com>
2019-02-08 12:04:58 +0100

    Change url pattern for students and teachers

--- school/urls.py
+++ school/urls.py
@@ -22,40 +22,40 @@ urlpatterns = [path("", include("school.dashboard.urls"))]
 
 urlpatterns += [
     path(
-        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+        "accounts/login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
     ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
-        "password_change/",
+        "accounts/password_change/",
         auth_views.PasswordChangeView.as_view(),
         name="password_change",
     ),
     path(
-        "password_change/done/",
+        "accounts/password_change/done/",
         auth_views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
     ),
     path(
-        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+        "accounts/password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
     ),
     path(
-        "password_reset/done/",
+        "accounts/password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(),
         name="password_reset_done",
     ),
     path(
-        "reset/<uidb64>/<token>/",
+        "accounts/reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
     path(
-        "reset/done/",
+        "accounts/reset/done/",
         auth_views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
 ]
 
 urlpatterns += [
-    path("accounts/", include("school.accounts.urls")),
+    path("", include("school.accounts.urls")),
     path("admin/", admin.site.urls),
 ]
