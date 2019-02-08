2e126ea7b592598cd6aa4e978f43cddfa8d787cd
Paweł Bednarz <pbedn@users.noreply.github.com>
2019-02-08 12:05:19 +0100

    Add test for unknown user

---
+++ school/accounts/tests/test_login.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+
+class UnknownUserTest(TestCase):
+    """
+    Test for Unknown User Login
+    """
+
+    def test_redirects_to_login_page_for_unknown_user(self):
+        url = '/'
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response,
+                             expected_url="{}?next={}".format(reverse('login'), url),
+                             target_status_code=200)
+        url = reverse('password_change')
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response,
+                             expected_url="{}?next={}".format(reverse('login'), url),
+                             target_status_code=200)
+        url = reverse('password_change_done')
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response,
+                             expected_url="{}?next={}".format(reverse('login'), url),
+                             target_status_code=200)
+        url = reverse('student_list')
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response,
+                             expected_url="{}?next={}".format(reverse('login'), url),
+                             target_status_code=200)
+        url = reverse('teacher_create')
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response,
+                             expected_url="{}?next={}".format(reverse('login'), url),
+                             target_status_code=200)
